32eb82f132deea8355ba259a9d77b3acc526cc70
aderp <aderahmanp@gmail.com>
2020-09-03 22:58:43 +0700

    Tugas-9

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,20 +5,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="container">
+        <h1 className="form"> <b>Form Pembeli Buah</b></h1>
+        <form>
+          <div className="pelanggan">
+            <label><b>nama Pelanggan</b></label>
+            <input className="input" type="text"></input>
+          </div>
+          
+          <div className="radio">
+            <input type="checkbox" id="semanka" name="semangka"></input>
+            <label for="semangka" >Semangka</label><br></br>
+            <input type="checkbox" id="jeruk" name="jeruk"></input>
+            <label for="jeruk">Jeruk</label><br></br>
+            <input type="checkbox" id="nanas" name="nanas"></input>
+            <label for="nanas">nanas</label><br></br>
+            <input type="checkbox" id="salak" name="salak"></input>
+            <label for="salak">salak</label><br></br>
+            <input type="checkbox" id="anggur" name="anggur"></input>
+            <label for="anggur">anggur</label><br></br>
+          </div>
+          <label><b>Daftar Item</b></label> <br></br>
+          <button className="button" type="button">Kirim</button>
+        </form>
+      </section>
     </div>
   );
 }
