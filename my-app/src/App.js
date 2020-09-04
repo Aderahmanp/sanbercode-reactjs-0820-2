52cf0f61d233480fe65a18d5393890ab15b801d3
aderp <aderahmanp@gmail.com>
2020-09-04 21:59:29 +0700

    create folder tugas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Form from './Tugas-9/tugas9'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <section className="container">
-        <h1 className="form"> <b>Form Pembeli Buah</b></h1>
-        <form>
-          <div className="pelanggan">
-            <label><b>Nama Pelanggan</b></label>
-            <input className="input" type="text"></input>
-          </div>
-          
-          <div className="radio">
-            <input type="checkbox" id="semanka" name="semangka"></input>
-            <label for="semangka" >Semangka</label><br></br>
-            <input type="checkbox" id="jeruk" name="jeruk"></input>
-            <label for="jeruk">Jeruk</label><br></br>
-            <input type="checkbox" id="nanas" name="nanas"></input>
-            <label for="nanas">nanas</label><br></br>
-            <input type="checkbox" id="salak" name="salak"></input>
-            <label for="salak">salak</label><br></br>
-            <input type="checkbox" id="anggur" name="anggur"></input>
-            <label for="anggur">anggur</label><br></br>
-          </div>
-          <label><b>Daftar Item</b></label> <br></br>
-          <button className="button" type="button">Kirim</button>
-        </form>
-      </section>
+      <Form/>
     </div>
   );
 }
---
+++ my-app/src/Tugas-9/tugas9.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+function form(){
+    return (
+        <section className="container">
+        <h1 className="form"> <b>Form Pembeli Buah</b></h1>
+        <form>
+          <div className="pelanggan">
+            <label><b>Nama Pelanggan</b></label>
+            <input className="input" type="text"></input>
+          </div>
+          
+          <div className="radio">
+            <input type="checkbox" id="semanka" name="semangka"></input>
+            <label for="semangka" >Semangka</label><br></br>
+            <input type="checkbox" id="jeruk" name="jeruk"></input>
+            <label for="jeruk">Jeruk</label><br></br>
+            <input type="checkbox" id="nanas" name="nanas"></input>
+            <label for="nanas">nanas</label><br></br>
+            <input type="checkbox" id="salak" name="salak"></input>
+            <label for="salak">salak</label><br></br>
+            <input type="checkbox" id="anggur" name="anggur"></input>
+            <label for="anggur">anggur</label><br></br>
+          </div>
+          <label><b>Daftar Item</b></label> <br></br>
+          <button className="button" type="button">Kirim</button>
+        </form>
+      </section>
+    )
+}
+
+export default form
